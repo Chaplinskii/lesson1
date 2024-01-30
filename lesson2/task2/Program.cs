d02114950cf78366bc83529ab0766f6e33766169
Chaplinskii <6945053@gmail.com>
2024-01-30 21:33:04 +0300

    задача 3 гири через циклы

--- lesson2/task2/Program.cs
+++ lesson2/task2/Program.cs
@@ -38,14 +38,36 @@
 // }
 
 // Задача 3 (гири)
-int n=5;
-int []array={9,-4,7,3,2};
-int i =0;
-int max=array[0];
-while (i<n){
-    if (array[i]>max){
-        max=array[i];
+int n = 5;
+int[] array = { 9, -4, 7, 3, 2 };
+int i = 0;
+int max = array[0];
+while (i < n)
+{
+    if (array[i] > max)
+    {
+        max = array[i];
     }
-    i=i+1;
+    i = i + 1;
 }
-Console.Write(max);
+Console.WriteLine(max);
+
+max = array[0];
+for (int j = 0; j < n; j++)
+{
+    if (array[j] > max)
+    {
+        max = array[j];
+    }
+}
+Console.WriteLine(max);
+
+max = array[0];
+foreach (int e in array)
+{
+    if (e > max)
+    {
+        max = e;
+    }
+}
+Console.WriteLine(max);
