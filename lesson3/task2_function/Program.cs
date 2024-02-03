a3fb3a47311bade1ad39ee3cfa2e16acf16dfa28
Chaplinskii <6945053@gmail.com>
2024-02-03 21:14:29 +0300

    add lesson3:task3

--- lesson3/task2_function/Program.cs
+++ lesson3/task2_function/Program.cs
@@ -1,4 +1,5 @@
-﻿// double CalculateFormula(int a, int b, int c, int d)
+﻿// Задача1
+// double CalculateFormula(int a, int b, int c, int d)
 // {
 //     double numenator = a * b;
 //     int denumenator = c + d;
@@ -9,16 +10,40 @@
 // double result = CalculateFormula(1, 2, 3, 4);
 // Console.WriteLine(result);
 
-void PrintSquares(int limit)
-{
-    int i =1;
-    while (i<=limit)
-    {
-Console.Write($"{i*i} ");
-i++;
-    }
-}
+// Задача2
+// void PrintSquares(int limit)
+// {
+//     int i =1;
+//     while (i<=limit)
+//     {
+// Console.Write($"{i*i} ");
+// i++;
+//     }
+// }
 
-PrintSquares(5);
-PrintSquares(10);
-PrintSquares(15);
+// PrintSquares(5);
+// PrintSquares(10);
+// PrintSquares(15);
+
+// Задача3
+// void ZeroEventElements(int[] arr)
+// {
+//     for (int i = 0; i < arr.Length; i++)
+//     {
+//         if (arr[i] % 2 == 0)
+//         {
+//             arr[i] = 0;
+//         }
+//     }
+// }
+// void PrintArray(int[] arr)
+// {
+//     foreach (int e in arr)
+//     {
+//         Console.Write($"{e} ");
+//     }
+// }
+// int [] array={1,2,3,4,5};
+// ZeroEventElements(array);
+// PrintArray(array);
+
