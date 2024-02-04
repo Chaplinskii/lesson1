1f3fd54a085dc56d84defdfda61bf1b215de1ca3
Chaplinskii <6945053@gmail.com>
2024-02-04 17:18:03 +0300

    add lesson4:task3

--- lesson4/task1/Program.cs
+++ lesson4/task1/Program.cs
@@ -32,60 +32,77 @@
 // ShowMatrix(matrix);
 
 // Задача2
-int[,] CreateMatrix(int rowCount, int columsCount)
+// int[,] CreateMatrix(int rowCount, int columsCount)
+// {
+//     int[,] matrix = new int[rowCount, columsCount];
+//     Random rnd = new Random();
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+
+//             matrix[i, j] = rnd.Next(1, 1000);
+
+//         }
+//     }
+//     return matrix;
+// }
+// void ShowMatrix(int[,] matrix)
+// {
+//     for (int i = 0; i < matrix.GetLength(0); i++)
+//     {
+//         for (int j = 0; j < matrix.GetLength(1); j++)
+//         {
+
+//             Console.Write($"{matrix[i, j]} ");
+
+//         }
+//         Console.WriteLine();
+//     }
+// }
+// int[,] matrix = CreateMatrix(3, 4);
+// ShowMatrix(matrix);
+// foreach (int e in matrix)
+// {
+//     if (IsInteresting(e) == true)
+//     {
+//         Console.WriteLine(e);
+//     }
+// }
+// bool IsInteresting(int value)
+// {
+//     int sumOfDigits = GetSumOfDigits(value);
+//     if (sumOfDigits % 2 == 0)
+//     {
+//         return true;
+//     }
+//     return false;
+// }
+// int GetSumOfDigits(int value)
+// {
+//     int sum = 0;
+
+//     while (value > 0)
+//     {
+//         sum = sum + value % 10;
+//         value = value / 10;
+//     }
+//     return sum;
+// }
+
+// Задача3
+string GetLettersString(string s)
 {
-    int[,] matrix = new int[rowCount, columsCount];
-    Random rnd = new Random();
-    for (int i = 0; i < matrix.GetLength(0); i++)
+    string letters = "";
+    foreach (char e in s)
     {
-        for (int j = 0; j < matrix.GetLength(1); j++)
+        if (char.IsAsciiLetter(e) == true)
         {
-
-            matrix[i, j] = rnd.Next(1, 1000);
-
+            letters = letters + e;
         }
     }
-    return matrix;
+    return letters;
 }
-void ShowMatrix(int[,] matrix)
-{
-    for (int i = 0; i < matrix.GetLength(0); i++)
-    {
-        for (int j = 0; j < matrix.GetLength(1); j++)
-        {
-
-            Console.Write($"{matrix[i, j]} ");
-
-        }
-        Console.WriteLine();
-    }
-}
-int[,] matrix = CreateMatrix(3, 4);
-ShowMatrix(matrix);
-foreach (int e in matrix)
-{
-    if (IsInteresting(e) == true)
-    {
-        Console.WriteLine(e);
-    }
-}
-bool IsInteresting(int value)
-{
-    int sumOfDigits = GetSumOfDigits(value);
-    if (sumOfDigits % 2 == 0)
-    {
-        return true;
-    }
-    return false;
-}
-int GetSumOfDigits(int value)
-{
-    int sum = 0;
-   
-    while (value > 0)
-    {
-        sum = sum + value % 10;
-        value = value / 10;
-    }
-    return sum;
-}
+string str = Console.ReadLine()!;
+string result = GetLettersString(str);
+Console.WriteLine(result);
